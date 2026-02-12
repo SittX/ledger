f264b49b808eb395a677e62a4b42d54dc755f3f9
Kyaw Sitt Thway <kyawsittthway@cbbank.com.mm>
2026-02-12 22:06:56 +0630

    Fixed (Layout import issues)

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import Sidebar from "@/components/ui/Sidebar";
-import Header from "@/components/ui/Navbar";
+import Sidebar from "@/components/ui/sidebar";
+import Header from "@/components/ui/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
