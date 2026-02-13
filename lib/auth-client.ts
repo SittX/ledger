d559853f26b7d4996fabfeba5a28e1e5f51212e4
Keev <thwaykyawsitt@yahoo.com>
2026-02-13 12:32:13 +0630

    Removed BETTER_AUTH_URL in the env.ts

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -4,6 +4,7 @@ const getBaseUrl = () => {
     if (process.env.VERCEL_URL) {
         return `https://${process.env.VERCEL_URL}`;
     }
+    console.log("Vercel URL not found. Fallback to local development URL.");
     return "http://localhost:3000";
 };
 
